9a5594ece412fe2b06943cdf26c72bda1a5ab059
Justin Mundt <JustinMundt@gmail.com>
2026-01-14 22:18:00 -0700

    Fix keymap discoverability and resolve conflicts
    Fixes:
    - Resolve <leader>hp conflict: move gitsigns preview hunk to <leader>gp
    - All leader keymaps now discoverable via which-key
    
    Gitsigns improvements (all under <leader>g):
    - <leader>gp: Preview hunk
    - <leader>gs/gr: Stage/reset hunk (works in visual mode too)
    - <leader>gS/gR: Stage/reset entire buffer
    - <leader>gu: Undo stage hunk
    - <leader>gb: Blame line (full)
    - <leader>gd/gD: Diff this / Diff this ~
    - <leader>gtb/gtd: Toggle line blame / Toggle deleted
    
    Which-key updates:
    - Add <leader>f group for [F]ind
    - Add <leader>gt group for [G]it [T]oggle
    - Register standalone keymaps: <leader><space>, /, ?, a, A, b, D, e, m, o, p, q

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -9,12 +9,16 @@ return {
       changedelete = { text = '~' },
     },
     on_attach = function(bufnr)
-      vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk,
-        { buffer = bufnr, desc = 'Preview git hunk' })
+      local gs = require('gitsigns')
 
-      -- don't override the built-in and fugitive keymaps
-      local gs = package.loaded.gitsigns
-      vim.keymap.set({ 'n', 'v' }, ']c', function()
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      -- Navigation (don't override built-in and fugitive keymaps in diff mode)
+      map({ 'n', 'v' }, ']c', function()
         if vim.wo.diff then
           return ']c'
         end
@@ -22,9 +26,9 @@ return {
           gs.next_hunk()
         end)
         return '<Ignore>'
-      end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
+      end, { expr = true, desc = 'Next git hunk' })
 
-      vim.keymap.set({ 'n', 'v' }, '[c', function()
+      map({ 'n', 'v' }, '[c', function()
         if vim.wo.diff then
           return '[c'
         end
@@ -32,7 +36,30 @@ return {
           gs.prev_hunk()
         end)
         return '<Ignore>'
-      end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
+      end, { expr = true, desc = 'Previous git hunk' })
+
+      -- Actions (under <leader>g for Git)
+      map('n', '<leader>gp', gs.preview_hunk, { desc = 'Git: Preview hunk' })
+      map('n', '<leader>gs', gs.stage_hunk, { desc = 'Git: Stage hunk' })
+      map('n', '<leader>gr', gs.reset_hunk, { desc = 'Git: Reset hunk' })
+      map('v', '<leader>gs', function()
+        gs.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+      end, { desc = 'Git: Stage hunk' })
+      map('v', '<leader>gr', function()
+        gs.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+      end, { desc = 'Git: Reset hunk' })
+      map('n', '<leader>gS', gs.stage_buffer, { desc = 'Git: Stage buffer' })
+      map('n', '<leader>gu', gs.undo_stage_hunk, { desc = 'Git: Undo stage hunk' })
+      map('n', '<leader>gR', gs.reset_buffer, { desc = 'Git: Reset buffer' })
+      map('n', '<leader>gb', function()
+        gs.blame_line({ full = true })
+      end, { desc = 'Git: Blame line' })
+      map('n', '<leader>gd', gs.diffthis, { desc = 'Git: Diff this' })
+      map('n', '<leader>gD', function()
+        gs.diffthis('~')
+      end, { desc = 'Git: Diff this ~' })
+      map('n', '<leader>gtb', gs.toggle_current_line_blame, { desc = 'Git: Toggle line blame' })
+      map('n', '<leader>gtd', gs.toggle_deleted, { desc = 'Git: Toggle deleted' })
     end,
   },
 }
--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -9,14 +9,30 @@ return {
     local wk = require('which-key')
     wk.setup({})
     wk.add({
+      -- Groups
       { '<leader>c', group = '[C]ode' },
       { '<leader>d', group = '[D]ebug' },
+      { '<leader>f', group = '[F]ind' },
       { '<leader>g', group = '[G]it' },
+      { '<leader>gt', group = '[G]it [T]oggle' },
       { '<leader>h', group = '[H]arpoon' },
       { '<leader>r', group = '[R]ename' },
       { '<leader>s', group = '[S]earch' },
       { '<leader>w', group = '[W]orkspace' },
-      { '<leader>p', group = '[P]aste keep original' },
+
+      -- Standalone keymaps (for discoverability)
+      { '<leader><space>', desc = 'Find buffers' },
+      { '<leader>/', desc = 'Fuzzy find in buffer' },
+      { '<leader>?', desc = 'Recent files' },
+      { '<leader>a', desc = 'Swap parameter next' },
+      { '<leader>A', desc = 'Swap parameter prev' },
+      { '<leader>b', desc = 'Buffer picker' },
+      { '<leader>D', desc = 'Type definition' },
+      { '<leader>e', desc = 'Floating diagnostic' },
+      { '<leader>m', desc = 'Marks picker' },
+      { '<leader>o', desc = 'Oil file explorer' },
+      { '<leader>p', desc = 'Paste (keep register)' },
+      { '<leader>q', desc = 'Diagnostics list' },
     })
   end,
 }
